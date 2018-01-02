c3d38162d575fe5544cae4621793ba08db42b1b2
Vipul Modi <vipulm@ntdev.microsoft.com>
2018-01-02 12:12:30 -0800

    Workaround for the Issue-590. Allow serialization of the top level parameters with empty data contract namespace. The real fix is to wrap the method parameters in a generated type.

--- src/Microsoft.ServiceFabric.Services.Remoting/V2/BasicDataContractSerializationProvider.cs
+++ src/Microsoft.ServiceFabric.Services.Remoting/V2/BasicDataContractSerializationProvider.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
 
 namespace Microsoft.ServiceFabric.Services.Remoting.V2
 {
+    using System;
+    using System.Collections.Generic;
     using System.IO;
     using System.Runtime.Serialization;
     using System.Xml;
@@ -55,7 +56,7 @@ namespace Microsoft.ServiceFabric.Services.Remoting.V2
 
             using (var stream = new MemoryStream())
             {
-                using (var writer = XmlDictionaryWriter.CreateBinaryWriter(stream))
+                using (var writer = new MessageBodyXmlDictionaryWriter(XmlDictionaryWriter.CreateBinaryWriter(stream)))
                 {
                     serializer.WriteObject(writer, serviceRemotingRequestMessageBody);
                     writer.Flush();
@@ -108,7 +109,7 @@ namespace Microsoft.ServiceFabric.Services.Remoting.V2
 
             using (var stream = new MemoryStream())
             {
-                using (var writer = XmlDictionaryWriter.CreateBinaryWriter(stream))
+                using (var writer = new MessageBodyXmlDictionaryWriter(XmlDictionaryWriter.CreateBinaryWriter(stream)))
                 {
                     serializer.WriteObject(writer, serviceRemotingRequestMessageBody);
                     writer.Flush();
@@ -133,6 +134,154 @@ namespace Microsoft.ServiceFabric.Services.Remoting.V2
             {
                 return (ServiceRemotingResponseMessageBody)this.serializer.ReadObject(reader);
 
+            }
+        }
+    }
+
+    class MessageBodyXmlDictionaryWriter : XmlDictionaryWriter
+    {
+        private readonly XmlDictionaryWriter wrapped;
+
+        public MessageBodyXmlDictionaryWriter(XmlDictionaryWriter wrapped)
+        {
+            this.wrapped = wrapped;
+        }
+
+        public override WriteState WriteState
+        {
+            get
+            {
+                return this.wrapped.WriteState;
+            }
+        }
+
+        public override void Flush()
+        {
+            this.wrapped.Flush();
+        }
+
+        public override string LookupPrefix(string ns)
+        {
+            return wrapped.LookupPrefix(ns);
+        }
+
+        public override void WriteBase64(byte[] buffer, int index, int count)
+        {
+            this.wrapped.WriteBase64(buffer, index, count);
+        }
+
+        public override void WriteCData(string text)
+        {
+            this.wrapped.WriteCData(text);
+        }
+
+        public override void WriteCharEntity(char ch)
+        {
+            this.wrapped.WriteCharEntity(ch);
+        }
+
+        public override void WriteChars(char[] buffer, int index, int count)
+        {
+            this.wrapped.WriteChars(buffer, index, count);
+        }
+
+        public override void WriteComment(string text)
+        {
+            this.wrapped.WriteComment(text);
+        }
+
+        public override void WriteDocType(string name, string pubid, string sysid, string subset)
+        {
+            this.wrapped.WriteDocType(name, pubid, sysid, subset);
+        }
+
+        public override void WriteEndAttribute()
+        {
+            this.wrapped.WriteEndAttribute();
+        }
+
+        public override void WriteEndDocument()
+        {
+            this.wrapped.WriteEndDocument();
+        }
+
+        public override void WriteEndElement()
+        {
+            this.wrapped.WriteEndElement();
+        }
+
+        public override void WriteEntityRef(string name)
+        {
+            this.wrapped.WriteEntityRef(name);
+        }
+
+        public override void WriteFullEndElement()
+        {
+            this.wrapped.WriteFullEndElement();
+        }
+
+        public override void WriteProcessingInstruction(string name, string text)
+        {
+            this.wrapped.WriteProcessingInstruction(name, text);
+        }
+
+        public override void WriteRaw(string data)
+        {
+            this.wrapped.WriteRaw(data);
+        }
+
+        public override void WriteRaw(char[] buffer, int index, int count)
+        {
+            this.wrapped.WriteRaw(buffer, index, count);
+        }
+
+        public override void WriteStartAttribute(string prefix, string localName, string ns)
+        {
+            this.wrapped.WriteStartAttribute(prefix, localName, ns);
+        }
+
+        public override void WriteStartDocument()
+        {
+            this.wrapped.WriteStartDocument();
+        }
+
+        public override void WriteStartDocument(bool standalone)
+        {
+            this.wrapped.WriteStartDocument(standalone);
+        }
+
+        public override void WriteStartElement(string prefix, string localName, string ns)
+        {
+            if (string.IsNullOrEmpty(prefix) && (ns == Constants.ServiceCommunicationNamespace))
+            {
+                prefix = "_x";
+            }
+
+            this.wrapped.WriteStartElement(prefix, localName, ns);
+        }
+
+        public override void WriteString(string text)
+        {
+            this.wrapped.WriteString(text);
+        }
+
+        public override void WriteSurrogateCharEntity(char lowChar, char highChar)
+        {
+            this.wrapped.WriteSurrogateCharEntity(lowChar, highChar);
+        }
+
+        public override void WriteWhitespace(string ws)
+        {
+            this.wrapped.WriteWhitespace(ws);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+
+            if (disposing)
+            {
+                wrapped.Dispose();
             }
         }
     }
--- src/Microsoft.ServiceFabric.Services.Remoting/V2/ServiceRemotingDataContractSerializationProvider.cs
+++ src/Microsoft.ServiceFabric.Services.Remoting/V2/ServiceRemotingDataContractSerializationProvider.cs
@@ -110,7 +110,7 @@ namespace Microsoft.ServiceFabric.Services.Remoting.V2
 
                 using (var stream = new SegmentedPoolMemoryStream(this.bufferPoolManager))
                 {
-                    using (var writer = XmlDictionaryWriter.CreateBinaryWriter(stream))
+                    using (var writer = new MessageBodyXmlDictionaryWriter(XmlDictionaryWriter.CreateBinaryWriter(stream)))
                     {
                         this.serializer.WriteObject(writer, serviceRemotingRequestMessageBody);
                         writer.Flush();
@@ -165,7 +165,7 @@ namespace Microsoft.ServiceFabric.Services.Remoting.V2
 
                 using (var stream = new SegmentedPoolMemoryStream(this.bufferPoolManager))
                 {
-                    using (var writer = XmlDictionaryWriter.CreateBinaryWriter(stream))
+                    using (var writer = new MessageBodyXmlDictionaryWriter(XmlDictionaryWriter.CreateBinaryWriter(stream)))
                     {
                         this.serializer.WriteObject(writer, serviceRemotingResponseMessageBody);
                         writer.Flush();
@@ -187,6 +187,154 @@ namespace Microsoft.ServiceFabric.Services.Remoting.V2
                 {
                     return (ServiceRemotingResponseMessageBody)this.serializer.ReadObject(reader);
 
+                }
+            }
+        }
+
+        class MessageBodyXmlDictionaryWriter : XmlDictionaryWriter
+        {
+            private readonly XmlDictionaryWriter wrapped;
+
+            public MessageBodyXmlDictionaryWriter(XmlDictionaryWriter wrapped)
+            {
+                this.wrapped = wrapped;
+            }
+
+            public override WriteState WriteState
+            {
+                get
+                {
+                    return this.wrapped.WriteState;
+                }
+            }
+
+            public override void Flush()
+            {
+                this.wrapped.Flush();
+            }
+
+            public override string LookupPrefix(string ns)
+            {
+                return wrapped.LookupPrefix(ns);
+            }
+
+            public override void WriteBase64(byte[] buffer, int index, int count)
+            {
+                this.wrapped.WriteBase64(buffer, index, count);
+            }
+
+            public override void WriteCData(string text)
+            {
+                this.wrapped.WriteCData(text);
+            }
+
+            public override void WriteCharEntity(char ch)
+            {
+                this.wrapped.WriteCharEntity(ch);
+            }
+
+            public override void WriteChars(char[] buffer, int index, int count)
+            {
+                this.wrapped.WriteChars(buffer, index, count);
+            }
+
+            public override void WriteComment(string text)
+            {
+                this.wrapped.WriteComment(text);
+            }
+
+            public override void WriteDocType(string name, string pubid, string sysid, string subset)
+            {
+                this.wrapped.WriteDocType(name, pubid, sysid, subset);
+            }
+
+            public override void WriteEndAttribute()
+            {
+                this.wrapped.WriteEndAttribute();
+            }
+
+            public override void WriteEndDocument()
+            {
+                this.wrapped.WriteEndDocument();
+            }
+
+            public override void WriteEndElement()
+            {
+                this.wrapped.WriteEndElement();
+            }
+
+            public override void WriteEntityRef(string name)
+            {
+                this.wrapped.WriteEntityRef(name);
+            }
+
+            public override void WriteFullEndElement()
+            {
+                this.wrapped.WriteFullEndElement();
+            }
+
+            public override void WriteProcessingInstruction(string name, string text)
+            {
+                this.wrapped.WriteProcessingInstruction(name, text);
+            }
+
+            public override void WriteRaw(string data)
+            {
+                this.wrapped.WriteRaw(data);
+            }
+
+            public override void WriteRaw(char[] buffer, int index, int count)
+            {
+                this.wrapped.WriteRaw(buffer, index, count);
+            }
+
+            public override void WriteStartAttribute(string prefix, string localName, string ns)
+            {
+                this.wrapped.WriteStartAttribute(prefix, localName, ns);
+            }
+
+            public override void WriteStartDocument()
+            {
+                this.wrapped.WriteStartDocument();
+            }
+
+            public override void WriteStartDocument(bool standalone)
+            {
+                this.wrapped.WriteStartDocument(standalone);
+            }
+
+            public override void WriteStartElement(string prefix, string localName, string ns)
+            {
+                if (string.IsNullOrEmpty(prefix) && (ns == Constants.ServiceCommunicationNamespace))
+                {
+                    prefix = "_x";
+                }
+
+                this.wrapped.WriteStartElement(prefix, localName, ns);
+            }
+
+            public override void WriteString(string text)
+            {
+                this.wrapped.WriteString(text);
+            }
+
+            public override void WriteSurrogateCharEntity(char lowChar, char highChar)
+            {
+                this.wrapped.WriteSurrogateCharEntity(lowChar, highChar);
+            }
+
+            public override void WriteWhitespace(string ws)
+            {
+                this.wrapped.WriteWhitespace(ws);
+            }
+
+            protected override void Dispose(bool disposing)
+            {
+                base.Dispose(disposing);
+
+                if (disposing)
+                {
+                    wrapped.Dispose();
                 }
             }
         }
